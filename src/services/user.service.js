0a8a5d5eeed4c56a45fcbcf1c8e2e9fe873da48f
DilemmaFixer3 <miroslava.babiychuk@gmail.com>
2022-06-28 16:47:28 +0300

    homework3.1

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -6,12 +6,6 @@ let axiosInstance = axios.create({
 
 const getUsers=()=> axiosInstance('/users').then(value => (value.data));
 
-
-// const getUser = (id)=>{
-//     axiosInstance.get('/users/'+id).then(value => value.data);
-//
-// }
-
 const getUsersPosts =(id)=> axiosInstance('users/'+id+'/posts').then(value => value);
 
 export {getUsers, getUsersPosts};
