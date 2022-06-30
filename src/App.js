63ec1a5ff66cd63364a291d46abe06a9ebe748f7
DilemmaFixer3 <miroslava.babiychuk@gmail.com>
2022-06-30 16:34:16 +0300

    homework4.1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import {useForm} from "react-hook-form";
+import {postUser} from "./services";
 
-function App() {
+export default function App() {
+
+  let {register, handleSubmit} = useForm({defaultValues:{ID:'0',
+      name:'name', username:'username', email:'email',
+      address:'address', phone:'phone', website:'website', company:'company'}});
+
+  let submit =(obj)=> {
+    console.log(obj);
+
+    postUser(obj);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <form onSubmit={handleSubmit(submit)}>
+        <input type="number" {...register("ID")}/>
+        <input type="text" {...register("name")}/>
+        <input type="text" {...register("username")}/>
+        <input type="email" {...register("email")}/>
+        <input type="text" {...register("address")}/>
+        <input type="tel" {...register("phone")}/>
+        <input type="url" {...register("website")}/>
+        <input type="text" {...register("company")}/>
+        <button>SAVE</button>
+
+      </form>
+
+
+
     </div>
   );
 }
 
-export default App;
